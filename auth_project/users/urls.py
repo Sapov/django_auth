7c97e8a0b1a124631af836ff75c8d8a477aa4eaf
Sapov <Sapov@mail.ru>
2023-01-22 19:12:39 +0300

    add registration 2

--- auth_project/users/urls.py
+++ auth_project/users/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
+from django.views.generic import TemplateView
+
 from . import views
 
 urlpatterns = [
-    path('', views.homePageView, name='home'),
+
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+
+    # path('', views.SignUpView.as_view(), name='signup'),
     path('signup/', views.SignUpView.as_view(), name='signup'),
     # path('signup/', views.SignUpView.as_view(), name='signup'),
 
